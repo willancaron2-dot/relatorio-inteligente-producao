78cf8593057ef584dc1757f412ad5554ec83c35e
willancaron2-dot <willancaron2@gmail.com>
2026-05-06 09:59:59 -0300

    Create index.js

---
+++ app/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:20}}>
+      <h1>🔥 Sistema online</h1>
+      <p>Se você está vendo isso, o deploy funcionou.</p>
+    </div>
+  );
+}
